23096f410d69455c89ada312164ed6bb90ec6733
priyanshu sahu <sahupriyanshu69@gmail.com>
2023-11-06 05:18:43 +0530

    server url

--- app/(dashboard)/schedular/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>schedular</div>
-  )
-}
-
-export default page
--- app/(dashboard)/whiteboards/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>whiteboard</div>
-  )
-}
-
-export default page
